ce03a51d729ca934e8fb1bf7de05a666f61f0ef9
Alex Aaron <alexfrancisaaron@gmail.com>
2025-06-17 16:33:40 -0500

    created getPlayers async func

--- index.js
+++ index.js
@@ -1,49 +1,124 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-const url = 'https://letterboxd.com/director/david-lynch/';
-const API_KEY = 'b8f921ba';
+const wsop = 'https://www.25kfantasy.com/players/';
 
-const getPostTitles = async () => {
+const players = [
+  {
+    name: 'Alex',
+    draftedPlayers: [
+      {
+        playerName: 'Bryce Yockey',
+        score: 0
+      },
+      {
+        playerName: 'Maxx Coleman',
+        score: 0
+      },
+      {
+        playerName: 'Dan Zack',
+        score: 0
+      },
+      {
+        playerName: 'John Hennigan',
+        score: 0
+      },
+      {
+        playerName: 'Sean Winter',
+        score: 0
+      },
+      {
+        playerName: 'Jason Koon',
+        score: 0,
+      },
+      {
+        playerName: 'Brian Hastings',
+        score: 0
+      },
+      {
+        playerName: 'Sergio Aido',
+        score: 0
+      },
+      {
+        playerName: 'Aleksejs Ponakovs',
+        score: 0
+      }
+    ]
+  }
+];
+
+const getPlayers = async () => {
 	try {
-		// Load David Lynch page
-		const { data } = await axios.get(url);
+		const { data } = await axios.get(wsop);
 
-		// Parse HTML with Cheerio
+		// Parse data with Cheerio
 		const $ = cheerio.load(data);
 
-    const $li = $(".poster-container");
-
-    $li.each((i, el) => {
-      console.log(el.attribs);
-    })
-
-		// Initialise empty data array
-		// const postTitles = [];
-
-		// // Iterate over all anchor links for the given selector and ....
-		// $('div > p.title > a').each((_idx, el) => {
-		// 	// .... extract for each the tag text and add it to the data array
-		// 	const postTitle = $(el).text()
-		// 	postTitles.push(postTitle)
-		// });
-
-		// Return the array with all titles
+		const $anchors = $("a");
+		
+		$anchors.each((index, value) => {
+			if ($(value).attr('href').includes('/players/player-profile/')){
+				console.log($(value).text());
+			}
+		})
 	} catch (error) {
 		throw error;
 	}
 };
 
-// getPostTitles();
+const getScores = async () => {
 
-const getMovie = async () => {
-	const movie = await fetch("http://www.omdbapi.com/?i=tt3896198&apikey=b8f921ba&s=Blue Velvet&y=1986").
-		then(res => res.json()).
-		then(res => 
-			console.log(res.Search[0])
-		);
-	return movie;
+	players.forEach((player => {
+		player.draftedPlayers.forEach(draftee => {
+			const query = draftee.playerName.toLowerCase().split(" ").join("-");
+			const playerURL = `https://www.25kfantasy.com/players/player-profile/${query}/`;
+			try {
+				const { data } = await axios.get(playerURL);
+				const $ = cheerio.load(data);
+		
+				const $td = $('tr').find('td');
+		
+				$td.each((index, value) => {
+					if (index === 2){
+						console.log($(value).text());
+					}
+				});
+				
+			} catch (error) {
+				throw error;
+			}
+		})
+	}));
+
+
+
+	
 }
 
-var movies = getMovie();
+
+// getPlayers();
+
+getScores('Viktor Blom');
+
+
+
+
+
+
+
+
+
+
+
+
+// const getMovie = async () => {
+// 	const movie = await fetch("http://www.omdbapi.com/?i=tt3896198&apikey=b8f921ba&s=Blue Velvet&y=1986").
+// 		then(res => res.json()).
+// 		then(res => 
+// 			console.log(res.Search[0])
+// 		);
+// 	return movie;
+// }
+
+// var movies = getMovie();
 
